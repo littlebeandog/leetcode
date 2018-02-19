98fe94c191d03c34917c3c7ab3185d843e7758cf
langlang <langlang@chuchujie.com>
2018-02-19 18:58:34 +0800

    1

---
+++ 144. Binary Tree Preorder Traversal/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack = [] if not root else [root]
+        res = []
+        while stack:
+            root = stack.pop()
+            res.append(root.val)
+            if root.right:
+                stack.append(root.right)
+            if root.left:
+                stack.append(root.left)
+        return res
