3caf0fb8365478b4587709db938722cf324c96c6
langlang <langlang@chuchujie.com>
2018-02-26 10:38:17 +0800

    1

---
+++ 128. Longest Consecutive Sequence/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+        max_len = 0
+        
+        for num in nums:
+            if num - 1 not in nums:
+                cur_len = 1
+                
+                while num + 1 in nums:
+                    cur_len += 1
+                    num += 1
+                
+                max_len = max(max_len, cur_len)
+        
+        return max_len
